444c2a4def471ae24f571040294d4b740de99665
kirill3266 <ka.kirafa@rambler.ru>
2020-10-30 04:22:35 +0300

    Final commit

--- isxod_2.c
+++ isxod_2.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
 
-//составление таблицы смещений
-void preBmBc(char* x, int m, int* bmBc) {
+void preBmBc(char* str, int substrl, int* d) {
 	int i;
-
-	for (i = 0; i < 256; ++i)
-		bmBc[i] = m;
-	for (i = 0; i < m - 1; ++i)
-		bmBc[x[i]] = m - i - 1;
-}
-
-//функци€ сравнени€ строк
-int memcmp(const void* buf1, const void* buf2, int count)
-{
-	if (!count)
-		return(0);
-
-	while (--count && *(char*)buf1 == *(char*)buf2) {
-		buf1 = (char*)buf1 + 1;
-		buf2 = (char*)buf2 + 1;
+	for (i = 0; i < 256; ++i) {
+		d[i] = substrl;
 	}
-
-	return(*((unsigned char*)buf1) - *((unsigned char*)buf2));
+	for (i = 0; i < substrl - 1; ++i)
+		d[str[i]] = substrl - 1-i;
 }
-
-//функци€ вы€снени€ длины строки
+ 
 int strlen(char* str) {
 	int i = 0;
-	for (i = 0 ; str[i] != '\n'; i++);
+	for (i = 0; str[i] != '\n'; i++);
 	return i;
 }
 
-int func(char* str, char* substr) {
-	int strl = strlen(str);
+void func(char* str, char* substr) {
 	int substrl = strlen(substr);
 	int d[256];
-	int j=0;
-	char c;
-	//преинициализаци€
-	preBmBc(str, strl, d);
-	//собственно, сам алгоритм
-	while (j <= substrl - strl) {
-		c = substr[j + strl - 1];
-		if ((str[strl-1]==c)&&(memcmp(str,substr+j,strl-1)==0))
-			return j;
-		j += d[c];	
+	preBmBc(substr, substrl, d);
+	for (int i = 0; i < (1000 - substrl) && str[i] != '\n';) {
+		int flag = 1;
+		for (int j = 0; substr[j] != '\n'; ++j) {
+			if ((str[i + j] != substr[j]) || (i + j > 999)) {
+				flag = 0;
+				break;
+			}
+		}	
+		if (flag) {
+			printf("%d ", i);
+		}
+		i += d[str[i + substrl - 1]];
 	}
 }
 
@@ -57,6 +43,6 @@ int main() {
 	char substr[1000];
 	fgets(substr, 1000, stdin);
 	printf("Vasha podstroka: %s\n", substr);
-	printf("Result: %d",-1* func(str, substr));
+	func(str, substr);
 	return 0;
 }
